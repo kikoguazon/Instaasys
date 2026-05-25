27fa674a04afc69bb44d8523512a3a4e008a494d
kikoguazon <kikoguazon@gmail.com>
2026-05-25 13:03:10 +0800

    fix whitenoise storage

--- instaasys/settings.py
+++ instaasys/settings.py
@@ -199,7 +199,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Allow same-origin iframes (needed to embed /admin/ inside the portal)
 X_FRAME_OPTIONS = 'SAMEORIGIN'
